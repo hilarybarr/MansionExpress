b3c95386f0b7ff0582159a7d3507d20a21424d58
Dylan Richards <drichards2013@gmail.com>
2014-08-16 17:21:56 -0400

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
 	has_many :bookings
-	has_many :mansions, through: :bookings         
+	has_many :mansions, through: :bookings      
 
 	has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>", :join => '50x50>' }
 	validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
