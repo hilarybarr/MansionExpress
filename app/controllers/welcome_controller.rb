4a7daaa48acec8425bae9523a444a282ee95d8cb
Antonio <rantoniops@gmail.com>
2014-08-16 23:46:56 -0400

    added extra methods for calculating matches in percentage and points

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,4 @@
 class WelcomeController < ApplicationController
   def index
-  	@user1 = User.first
-  	@user2 = User.last
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-	def user_to_user_compatibility(user1, user2)
+	def user_to_user_compatibility_points(user1, user2)
 	points = 0
 	user1.personality_traits.each do |user1trait|
 		user2.personality_traits.each do |user2trait|
@@ -11,57 +11,58 @@ module ApplicationHelper
 	return points
 end
 
+
+	def user_to_user_compatibility_percentage(user1, user2)
+	points = 0
+	user1.personality_traits.each do |user1trait|
+		user2.personality_traits.each do |user2trait|
+			if user2trait == user1trait
+				points += 1
+			end
+		end
+	end
+	percentage = (points * 100) / PersonalityTrait.all.count
+	return percentage
+end
+
+
 # pledge is the pledge object where everybody who applies to the mansion is
 
-def overall_pledge_compatibility(pledge)
-	puts "start of method"
+def overall_pledge_compatibility_points(pledge)
 	total_points = 0
 	mansion_mates = pledge
-	puts "assigned mansion mates to pledge"
 	mansion_mates.each_with_index do |mate, index|
-		puts "iterating" 
 		index_of_next_mate = index + 1
-		puts index_of_next_mate 
 		next_mate_to_compare_with = mansion_mates[index_of_next_mate]
-		puts next_mate_to_compare_with
-		puts "HHHHHHHHHHHH"
 		until next_mate_to_compare_with == nil
-			puts "start of until bla bla"
-			points = user_to_user_compatibility(mate, next_mate_to_compare_with)
-			puts points
+			points = user_to_user_compatibility_points(mate, next_mate_to_compare_with)
 			total_points += points
-			puts total_points
 			next_mate_to_compare_with = mansion_mates[index_of_next_mate += 1]
-			puts "CHICHI"
-			puts index_of_next_mate
-			puts "WAKAKAKAKAKAKAKAAK"
-			puts next_mate_to_compare_with
-			puts "WAKAKAKAKAKAKAKAAK"
 		end
 	end
 	return total_points
-	puts "end of method"
+end
+
+
+def overall_pledge_compatibility_percentage(pledge)
+	total_points = 0
+	mansion_mates = pledge
+	mansion_mates.each_with_index do |mate, index|
+		index_of_next_mate = index + 1
+		next_mate_to_compare_with = mansion_mates[index_of_next_mate]
+		until next_mate_to_compare_with == nil
+			points = user_to_user_compatibility_points(mate, next_mate_to_compare_with)
+			total_points += points
+			next_mate_to_compare_with = mansion_mates[index_of_next_mate += 1]
+		end
+	end
+	percentage = (total_points * 100) / (PersonalityTrait.all.count * mansion_mates.count)
+	return percentage
 end
 
 end
 
 
-# def overall_pledge_compatibility(pledge)
-# 	total_points = 0
-# 	mansion_mates = pledge.users
-# 	mansion_mates.each_with_index do |mate, index|
-# 		index_of_next_mate = index + 1
-# 		next_mate_to_compare_with = mansion_mates[index_of_next_mate]
-# 		until next_mate_to_compare_with == nil
-# 			points = user_to_user_compatibility(mate, next_mate_to_compare_with)
-# 			total_points += points
-# 			next_mate_to_compare_with = mansion_mates[index_of_next_mate + 1]
-# 		end
-# 		return total_points
-# 	end
-# end
-
-# end
 
 
 
