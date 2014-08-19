bd549bdf8fa16a1eebc767ce5154f0c09cd9d16c
hilarybarr <hilaryrosebarr@gmail.com>
2014-08-19 11:07:31 -0400

    Pulled in welcome index refactoring

--- app/helpers/mansions_helper.rb
+++ app/helpers/mansions_helper.rb
@@ -8,4 +8,32 @@ module MansionsHelper
 		mansion.users.count < mansion.bedrooms
 	end
 
+	def prices_per_occupants(mansion)
+	  #  x=1
+		 # until x > mansion.bedrooms.to_i 
+		 # 	price=(mansion.price.scan(/[.0-9]/).join().to_i)/(x)
+		 #   print "#{x} People: #{price}" 
+		 #   x+=1
+		 #  end
+
+		 price(mansion)/occupants()
+		end
+
+
+	def occupants(mansion)
+		mansion.bedrooms.count
+	end
+
+
+	def price(mansion)
+		mansion.price.scan(/[.0-9]/).join().to_i
+	end
+
+
+		# bedrooms=mansion.bedrooms.to_i
+		# price= mansion.price.scan(/[.0-9]/).join().to_i)
+
+
+
+
 end
