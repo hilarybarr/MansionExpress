7a2e100c1e4234ab10363e1b9e17ee7ace21b30e
hilarybarr <hilaryrosebarr@gmail.com>
2014-08-19 10:47:26 -0400

    Created in_mansion? and room_available? helper methods

--- app/helpers/mansions_helper.rb
+++ app/helpers/mansions_helper.rb
@@ -1,2 +1,11 @@
 module MansionsHelper
+
+	def in_mansion? (user, mansion)
+		user.mansions.include?(mansion)
+	end
+
+	def room_available?(mansion)
+		mansion.users.count < mansion.bedrooms
+	end
+
 end
