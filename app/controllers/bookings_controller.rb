e70178d92b6189f8ee9035f0b642819cfa5ced91
Antonio <rantoniops@gmail.com>
2014-08-19 17:27:10 -0400

    fixed application form by removing potential political incorrect stuff, removed them from seed file, changed controller logic to prevent creating new mansions instead of updating them

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,30 +1,28 @@
 class BookingsController < ApplicationController
 
 	def create
-		mansion = Mansion.find(params[:id])
 		@user = User.create(params[:info])
 		sign_in @user
+		mansion = Mansion.find(params[:id])
+		Booking.where(user_id: @user.id, mansion_id: mansion.id).first_or_create
+		session[:user_id] = @user.id
 		@traits = params[:trait]
 		@traits.each do |trait|
-			if trait
+			if trait.name
 				@user.personality_traits << PersonalityTrait.find_by(name: trait)
 			end
 		end
-		Booking.create(user_id: @user, mansion_id: mansion)
-		session[:user_id] = @user.id
 		redirect_to show_booking_path
 	end
 
-		
 
 	def show
-		#add current user logic
-		@user = User.last # temporary fix cuz current_user is not working in the view
+		@user = User.find(session[:user_id])
 		@users = Booking.find_by(mansion_id: params[:id]).mansion.users
-		#removing the user from the users pledge so that when we compare for percentage the same user isnt comparing to himself
-		@mates = [] # the mates exclude the user that is comparing
-		@users.each do |user|
-			@mates << user unless user == @user
+		# we remove the user from the users pledge so that when we compare for percentage the same user isnt comparing to himself, the variable mates exclude the user that is comparing
+		@mates = [] 
+		@users.each do |mate|
+			@mates << mate unless mate == @user
 		end
 	end
 
