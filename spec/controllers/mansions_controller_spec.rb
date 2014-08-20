1d10494261c7141fe6f2e9eabedd6afaa690005b
Dylan Richards <drichards2013@gmail.com>
2014-08-20 08:09:31 -0400

    controller specs

--- spec/controllers/mansions_controller_spec.rb
+++ spec/controllers/mansions_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe MansionsController do
+  describe '#index' do
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+
 
 end
